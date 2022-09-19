6b7a361321f278488083a2f6ce21055e116a4aef
SergeyHodak <87858809+SergeyHodak@users.noreply.github.com>
2022-09-19 18:02:42 +0300

    finish

--- src/main/java/resume/baseservices/base_controller/BaseController.java
+++ src/main/java/resume/baseservices/base_controller/BaseController.java
@@ -10,15 +10,28 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Objects;
 
 @Controller
 public class BaseController {
     @GetMapping("/")
     public String getResumePage(Model model) {
+        File urlProjectClass = new File(Objects.requireNonNull(this.getClass().getResource("")).getPath());
+        String urlProject = urlProjectClass.toString().replace("\\", "/");
+        String[] folders = urlProject.split("/");
+        StringBuilder resultURL = new StringBuilder();
+        for (String folder : folders) {
+            if (Objects.equals(folder, "Resume")) {
+                resultURL.append(folder).append("/src/main/resources/static/");
+                break;
+            } else {
+                resultURL.append(folder).append("/");
+            }
+        }
         String filename = "images/Serge.jpg";
-        String fileURL = "D:/JavaSerge/Resume/src/main/resources/static/" + filename;
-        int requiredImageHeight = 300;
-        int width = 0;
+        String fileURL = resultURL.append(filename).toString();
+        int requiredImageHeight = 376;
+        int width;
         BufferedImage img;
         try {
             img = ImageIO.read(new File(new URI(fileURL).getPath()));
